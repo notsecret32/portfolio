cba5bfd478d9a78348d298a5e62d6f6e83e20bc8
Alexander Skvorchevskiy <alex.skvorchevsky@gmail.com>
2025-04-27 02:09:51 +0300

    feat: added theme switch support

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Fira_Code } from 'next/font/google';
 
+import { ThemeProvider } from '@/components/theme-provider';
 import './globals.css';
 
 const firaCode = Fira_Code({
@@ -20,8 +21,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${firaCode.variable} antialiased`}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${firaCode.variable} antialiased`}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='dark'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
